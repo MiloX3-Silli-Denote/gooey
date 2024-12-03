006064beae837801cc23528c1467e4f49cf15e13
Milo:3 Silli Denote <71358745+MiloX3-Silli-Denote@users.noreply.github.com>
2024-12-03 12:23:51 -0500

    Create component_base.lua

---
+++ component_base.lua
@@ -0,0 +1,51 @@
+-- replace all uses of 'ELEMENT_NAME' with the name of the element you are creating
+
+local ELEMENT_NAME = {};
+
+ELEMENT_NAME.__index = ELEMENT_NAME;
+
+function ELEMENT_NAME.new(container, x, y, w, h) -- add as many additional arguments as you want
+  assert(isNum(x, y, w, h), "bad arguments to create 'ELEMENT_NAME'");
+
+	local instance = setmetatable({}, ELEMENT_NAME);
+
+	instance.x = x;
+	instance.y = y;
+	instance.w = w;
+	instance.h = h;
+
+  -- set key of instance of however many inputs you need
+
+	container:addElement(instance);
+	container:claimIn(instance, x, y, x + w, y + h);
+
+	return instance;
+end
+
+function ELEMENT_NAME:gainFocus() -- called when this element gets clicked nearby
+end
+function ELEMENT_NAME:loseFocus() -- called when this element had focus (from above function) but was clicked off of
+end
+
+function ELEMENT_NAME:textInput(text)
+end
+function ELEMENT_NAME:keyPressed(key, isRepeat)
+end
+function ELEMENT_NAME:keyReleased(key)
+end
+
+function ELEMENT_NAME:mouseMoved(x, y)
+end
+function ELEMENT_NAME:mousePressed(x, y, button, presses)
+end
+function ELEMENT_NAME:mouseReleased(x, y, button, presses)
+end
+function ELEMENT_NAME:wheelMoved(x, y)
+end
+
+function ELEMENT_NAME:update(dt)
+end
+function ELEMENT_NAME:draw()
+end
+
+return setmetatable({}, {__call = function(_, ...) return ELEMENT_NAME.new(...) end});
